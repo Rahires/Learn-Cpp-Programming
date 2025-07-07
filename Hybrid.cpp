539d328ccb692840d1f7efebe9f0bb5b49e93ad3
Sahebrao Shivaji Rahire <rahires818@gmail.com>
2025-07-07 17:19:31 +0530

    Create Hybrid.cpp

---
+++ Hybrid.cpp
@@ -0,0 +1,43 @@
+//Hybrid inheritance program and virtual class program 
+#include<iostream>
+using namespace std;
+class GrandFather
+{
+public :
+void set()
+{
+cout<<"\nI have 20 ekar land and 2 kg gold.....";
+}
+};
+virtual class Father : public GrandFather
+{
+public :
+void get()
+{
+cout<<"\nI have 10 ekar land and 1 kg gold.....";
+}
+};
+virtual class Fathers_Sister :public GrandFather
+{
+void wait()
+{
+cout<<"\nI Have also 10 ekar land and 1 kg gold.....";
+}
+};
+class Child :public Father ,public Fathers_Sister
+{
+void Test()
+{
+cout<<"\nI have 20 ekar land and 2 kg gold.....";
+}
+};
+int main()
+{
+  Child C;
+C.set();
+C.get();
+C.wait();
+C.Test();
+return 0;
+}
+
