a4e840e481fbbda6fabea673f4cfc611d5928006
Sahebrao Shivaji Rahire <rahires818@gmail.com>
2025-07-08 23:19:59 +0530

    Create Virtual.cpp

---
+++ Virtual.cpp
@@ -0,0 +1,24 @@
+//Virtual function program
+#include<iostream>
+using namespace std;
+class Parent
+{
+public:
+virtual void CompleteGraduation()
+{
+cout<<"\nIN MBBS....";
+}
+};
+class Child :public Parent 
+{
+void CompleteGraduation()
+{
+cout<<"\nIN BCS...";
+}
+};
+int main()
+{
+Child C;
+C.CompleteGraduation();
+return 0;
+}
