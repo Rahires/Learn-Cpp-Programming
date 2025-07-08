0fdf79d05882d307bcd6c70e4b5e70b98dfba572
Sahebrao Shivaji Rahire <rahires818@gmail.com>
2025-07-08 23:06:58 +0530

    Create Constructor.cpp

---
+++ Constructor.cpp
@@ -0,0 +1,15 @@
+//Constructor program
+#include<iostream>
+using namespace std;
+class Sample
+{
+Sample()
+{
+  cout<<"\nHello World.....";
+}
+};
+int main()
+{
+Sample S=new Sample();
+return 0;
+}
